60d04b53dca75b08277368e026802e83f46ad8f8
PBillingsby <philbillingsby@gmail.com>
2024-03-21 14:33:25 -0400

    add refactored starting code with new helpers

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import { AccountState, TOKEN_2022_PROGRAM_ID, getAccount, mintTo, thawAccount, transfer, createAccount } from "@solana/spl-token";
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
-import { initializeKeypair } from "./keypair-helpers";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { createTokenExtensionMintWithDefaultState } from "./mint-helper";
+import { initializeKeypair, makeKeypairs } from '@solana-developers/helpers';
 
 (async () => {
   const connection = new Connection("http://127.0.0.1:8899", "confirmed");
-  const payer = await initializeKeypair(connection);
-  const mintKeypair = Keypair.generate();
+  const payer = await initializeKeypair(connection, {
+    airdropAmount: 1 * LAMPORTS_PER_SOL
+  });
+
+  const [mintKeypair, ourTokenAccountKeypair, otherTokenAccountKeypair] = makeKeypairs(3)
   const mint = mintKeypair.publicKey;
   const decimals = 2;
   const defaultState = AccountState.Frozen;
 
-  const ourTokenAccountKeypair = Keypair.generate();
   const ourTokenAccount = ourTokenAccountKeypair.publicKey;
 
   // To satisfy the tranferring tests
-  const otherTokenAccountKeypair = Keypair.generate();
   const otherTokenAccount = otherTokenAccountKeypair.publicKey;
 
   const amountToMint = 1000;
